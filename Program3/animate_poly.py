3d60eedc1c95bc6e980c020e8fc19ef96e5fcc65
csos95 <csoscss@gmail.com>
2015-10-01 20:16:32 -0500

    Add comments and change collision method
    Add comments to class methods and change the polygon
    collision method to first check if the polygon mbr's intersect
    and only do the more accurate bruteforce method if they do

--- Program3/animate_poly.py
+++ Program3/animate_poly.py
@@ -1,9 +1,16 @@
-"""Point and Rectangle classes.
+"""
+@author - Christopher Silva
+@date -  10/1/2015
+@description - This program uses pantograph to draw three
+polygons and multiple points to an html canvas that can
+be viewed in a browser at 127.0.0.1:8080
+when polygons collide they bounce off of each other
+when a point is within a polygon the point turns red
 
-This code is in the public domain.
-
-Point  -- point with (x,y) coordinates
-Rect  -- two points, forming a rectangle
+@resources - I modified the base geo.py and used the 
+bouncing shape class from the pantograph bouncingball
+example as a starting point for my BouncingShape and
+BouncingPoint classes
 """
 import pantograph
 import math
@@ -35,6 +42,7 @@ class Point:
         return (self.x, self.y)
 
 class BouncingPoint:
+    """initializes the values for the bouncing shape"""
     def __init__(self, point, size):
         self.x = point[0]
         self.y = point[1]
@@ -42,7 +50,8 @@ class BouncingPoint:
         self.size = size
         self.xvel = random.randint(1, 5)
         self.yvel = random.randint(1, 5)        
-        
+
+    """updates the position of the BouncingPoint"""
     def update(self, canvas):
         
         if self.x <= 0 or self.x >= canvas.width:
@@ -62,13 +71,14 @@ class BouncingPoint:
         
 
 class BouncingShape(object):
+    """initializes the values for the BouncingShape"""
     def __init__(self, shape):
         self.shape = shape
         self.xvel = random.randint(1, 5)
         self.yvel = random.randint(1, 5)
         self.theta = 0
-        self.rvel = 0#(math.pi / 2) * random.random()
 
+    """updates the position of the BouncingShape"""
     def update(self, canvas):
         rect = self.shape.get_bounding_rect()
 
@@ -79,14 +89,12 @@ class BouncingShape(object):
             self.yvel *= -1
             self.shape.translate(self.xvel, self.yvel)
 
-        self.theta += self.rvel
-        if self.theta > math.pi:
-            self.theta -= 2 * math.pi
-
         self.shape.translate(self.xvel, self.yvel)
         self.shape.rotate(self.theta)
         self.shape.draw(canvas)
-        
+       
+    """checks if a given point is within the Bouncing shape
+    and returns true/false"""
     def point_inside_polygon(self, p):
         
         if type(self.shape) is not pantograph.shapes.Polygon:
@@ -110,7 +118,7 @@ class BouncingShape(object):
         return inside
         
 class Driver(pantograph.PantographHandler):
-
+    """creates the BouncingShapes and BouncingPoints that will be displayed on the canvas"""
     def setup(self):
         self.static_points = []
         self.static_shapes = [pantograph.Polygon([(65, 51),  (90, 74),  (145, 60),  (201, 69),  (265, 46),  (333, 61),  (352, 99),  (370, 129),  (474, 138),  (474, 178),  (396, 225),  (351, 275),  (376, 312),  (382, 356),  (338, 368),  (287, 302),  (224, 304),  (128, 338),  (110, 316),  (129, 270),  (83, 231),  (103, 201),  (126, 162),  (83, 163)], None, "#00f")]
@@ -123,29 +131,37 @@ class Driver(pantograph.PantographHandler):
         self.points = [BouncingPoint(shp, 10) for shp in self.static_points]
         self.shapes = [BouncingShape(shp) for shp in self.static_shapes]
 
+    """this clears the screen, checks for collisions, and calls the update
+    method for all of the polygons and points"""
     def update(self):
         self.clear_rect(0, 0, self.width, self.height)
-
+        """checking for points inside of polygons"""
         for shape in self.shapes:
             for point in self.points:
                 if shape.point_inside_polygon(point):
                     point.color = "#f00"
-            
+        
+        """calling update methods for points and then resetting the color to green"""
         for point in self.points: 
             point.update(self)
             point.color = "#0f0"
 
+        """checking for collision between polygons and calling update methods for polygons
+        for collision it first checks if the mbr's of the polygons overlap and if they do it
+        does a more accurate brute force collision method. this saves some processing power
+        in that you don't need to compare every combination of points in all polygons each frame"""
         for a in self.shapes:
             for b in self.shapes:
                 if a != b:
-                    for point in b.shape.points:
-                        if a.point_inside_polygon(Point(point[0], point[1])):
-                            xveltmp = a.xvel
-                            yveltmp = a.yvel
-                            a.xvel = b.xvel
-                            a.yvel = b.yvel
-                            b.xvel = xveltmp
-                            b.yvel = yveltmp
+                    if a.shape.intersects(b.shape):
+                        for point in b.shape.points:
+                            if a.point_inside_polygon(Point(point[0], point[1])):
+                                xveltmp = a.xvel
+                                yveltmp = a.yvel
+                                a.xvel = b.xvel
+                                a.yvel = b.yvel
+                                b.xvel = xveltmp
+                                b.yvel = yveltmp
             a.update(self)
 
 if __name__ == '__main__':
